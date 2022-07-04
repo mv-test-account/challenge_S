21510826ce410b472cfa4c7b6bf95326813dd131
michalvenos <3Phobos3>
2022-07-04 16:28:24 +0200

    update action

--- cypress.config.js
+++ cypress.config.js
@@ -16,11 +16,13 @@ module.exports = defineConfig({
     charts: true,
     reportPageTitle: 'custom-title',
     embeddedScreenshots: true,
+    saveAllAttempts: false,
   },
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       return require('./cypress/plugins/index.js')(on, config)
     },
     defaultCommandTimeout: 10000,
